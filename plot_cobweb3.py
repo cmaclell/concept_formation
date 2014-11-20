1700cfacc0c1bbbba2fecc877dc7004789f6c135
Chris MacLellan <maclellan.christopher@gmail.com>
2014-11-20 10:27:14 -0500

    improved drawing rate, by not redrawing all points each time

--- plot_cobweb3.py
+++ plot_cobweb3.py
@@ -9,16 +9,17 @@ tree = Cobweb3Tree()
 
 num_clusters = 4 
 num_samples = 30
-sigma = 0.3
+sigma = 1
 
-xmean = [uniform(-3, 3) for i in range(num_clusters)]
-ymean = [uniform(-3, 3) for i in range(num_clusters)]
+xmean = [uniform(-8, 8) for i in range(num_clusters)]
+ymean = [uniform(-8, 8) for i in range(num_clusters)]
 label = ['bo', 'go', 'ro', 'co', 'mo', 'yo', 'ko']
 shuffle(label)
 label = label[0:num_clusters]
 
 data = []
 actual = []
+clusters = []
 for i in range(num_clusters):
     data += [{'x': normalvariate(xmean[i], sigma), 'y':
               normalvariate(ymean[i], sigma), '_label': label[i]} for j in
@@ -31,10 +32,20 @@ trained = []
 plt.ion()
 plt.show()
 
+# draw the actual sampling distribution
+for c in actual:
+    c.set_alpha(0.08)
+    c.set_facecolor("blue")
+    plt.gca().add_patch(c)
+
 for datum in data:
     #train the tree on the sampled datum
     tree.ifit(datum)
     trained.append(datum)
+
+    # remove old cluster circles
+    for c in clusters:
+        c.remove()
 
     # 4 * std gives two std on each side (~95% confidence)
     clusters = [Ellipse([cluster.av_counts['x'].unbiased_mean(),
@@ -43,27 +54,18 @@ for datum in data:
                         height=4*cluster.av_counts['y'].unbiased_std(), 
                         angle=0) for cluster in tree.root.children]
 
-    plt.cla()
-
-    # draw the actual sampling distribution
-    for c in actual:
-        c.set_alpha(0.08)
-        c.set_facecolor("blue")
-        plt.gca().add_patch(c)
-
     # draw the cluster circles
     for c in clusters:
         c.set_alpha(0.1)
         c.set_facecolor('red')
         plt.gca().add_patch(c)
 
-    # draw the points
-    for d in trained:
-        plt.plot([d['x']], [d['y']], d['_label'])
+    # draw the new point
+    plt.plot([datum['x']], [datum['y']], datum['_label'])
 
-    plt.axis([-5, 5, -5, 5])
+    plt.axis([-10, 10, -10, 10])
     plt.draw()
-    time.sleep(0.001)
+    time.sleep(0.0001)
 
 plt.ioff()
 plt.show()
