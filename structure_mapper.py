2ab906d1ae17782017de3f1ad0e239d06ce4fe02
Chris MacLellan <maclellan.christopher@gmail.com>
2014-11-29 12:42:55 -0500

    added standardize names

--- structure_mapper.py
+++ structure_mapper.py
@@ -1,7 +1,116 @@
+import re
 from search import DepthFGS
 
+gensym_counter = 0;
+
+def gensym():
+    global gensym_counter
+    gensym_counter += 1
+    return 'o' + str(gensym_counter)
+
 def standardizeApartNames(instance):
-    pass
+    """
+    Given an input instance (relations are still lists), it renames all the
+    objects so they have unique names.
+    """
+    new_instance = {}
+    relations = []
+    mapping = {}
+    for attr in instance:
+        if isinstance(instance[attr], list):
+            relations.append((attr, instance[attr]))
+        elif isinstance(instance[attr], dict):
+            mapping[attr] = gensym()
+            new_instance[mapping[attr]] = standardizeApartNames(instance[attr])
+        else:
+            new_instance[attr] = instance[attr]
+
+    for name, r in relations:
+        new_relation = []
+        for i,v in enumerate(r):
+            if i == 0:
+                new_relation.append(v)
+            else:
+                new_relation.append(mapping[v])
+        new_instance[name] = new_relation
+
+    return new_instance
+
+def getComponentNames(instance):
+    """
+    Given a flat representation of an instance or concept.av_counts
+    return a list of all of the component names.
+    """
+    names = set()
+    for attr in instance:
+        if isinstance(attr, tuple):
+            continue
+        attr = re.sub("_", "", attr)
+        for name in attr.split(".")[:-1]:
+            names.add(name)
+    return list(names)
+
+def rename(instance, mapping):
+    """
+    Given a flattened instance and a mapping (type = dict) rename the
+    components and relations and return the renamed instance.
+    """
+    # Ensure it is a complete mapping
+    # Might be troublesome if there is a name collision
+    for attr in instance:
+        #TODO need to handle ignores better.
+        if attr[0] == "_":
+            continue
+        if isinstance(attr, tuple):
+            continue
+        for name in attr.split('.')[:-1]:
+            if name not in mapping:
+                mapping[name] = name
+
+    temp_instance = {}
+    relations = []
+
+    # rename all attribute values
+    for attr in instance:
+        if isinstance(attr, tuple):
+            relations.append(attr)
+        elif attr.split('.')[:-1]:
+            ignore = False
+            if attr[0] == "_":
+                ignore = True
+            pattr = re.sub("_", "", attr)
+            new_attr = []
+            for name in pattr.split('.')[:-1]:
+                new_attr.append(mapping[name])
+            new_attr.append(pattr.split('.')[-1])
+            if ignore:
+                temp_instance["_" + "._".join(new_attr)] = instance[attr]
+            else:
+                temp_instance[".".join(new_attr)] = instance[attr]
+        else:
+            temp_instance[attr] = instance[attr]
+
+    #rename relations and add them to instance
+    for relation in relations:
+        temp = []
+        for idx, val in enumerate(relation):
+            if idx == 0:
+                temp.append(val)
+            else:
+                new_attr = []
+                for name in val.split("."):
+                    new_attr.append(mapping[name])
+                temp.append(".".join(new_attr))
+        temp_instance[tuple(temp)] = instance[relation]
+
+    #print(instance)
+    #print(mapping)
+    #print(temp_instance)
+    #print(relations)
+    #print()
+
+    return temp_instance
+
 
 def flattenJSON(instance):
     """
@@ -9,6 +118,7 @@ def flattenJSON(instance):
     hierarchy with periods in variable names. It also converts the 
     relations into tuples with values.
     """
+    instance = standardizeApartNames(instance)
     temp = {}
     for attr in instance:
         if isinstance(instance[attr], dict):
@@ -23,7 +133,7 @@ def flattenJSON(instance):
                             relation.append(attr + "." + val)
                     temp[tuple(relation)] = True
                 elif so_attr[0] == "_":
-                    # propagate attribute ignore up.
+                    # propagate ignore up.
                     temp["_" + attr + "." + so_attr] = subobject[so_attr]
                 else:
                     temp[attr + "." + so_attr] = subobject[so_attr]
@@ -34,20 +144,69 @@ def flattenJSON(instance):
                 temp[attr] = instance[attr]
     return temp
 
-def structurizeJSON(data):
+def traverseStructure(path, instance):
+    """
+    Given an instance (hashmap) to the given subobject for the given path.
+    Creates subobjects if they do not exist.
+    """
+    curr = instance
+    for obj in path:
+        if obj not in curr:
+            curr[obj] = {}
+        curr = curr[obj]
+    return curr
+
+def structurizeJSON(instance):
     """
     Takes a flattened instance and adds the structure back in. This essentially
-    "undoes" the flattening process.
+    "undoes" the flattening process. 
     """
-    pass
+    temp = {}
+    for attr in instance:
+        if isinstance(attr, tuple):
+            #attr.split('.')[:-1]
+            relation = []
+            path = []
+            for i,v in enumerate(attr):
+                if i == 0:
+                    relation.append(v)
+                else:
+                    path = v.split('.')
+                    relation.append(path[-1])
+                    path = path[:-1]
+            obj = traverseStructure(path, temp)
+            obj[tuple(relation)] = True
+
+        elif "." in attr:
+            # handle ignore
+            ignore = ""
+            if attr[0] == "_":
+                ignore = "_"
+
+            pattr = re.sub("_", "", attr)
+            path = pattr.split('.')
+            subatt = path[-1]
+            path = path[:-1]
+            curr = traverseStructure(path, temp)
+            curr[ignore + subatt] = instance[attr]
+
+        else:
+            temp[attr] = instance[attr]
+
+    return temp
 
 def mapStructure(concept, instance):
     pass
 
 if __name__ == "__main__":
 
-    o = {'o1': {'x':1, 'y': 1}, 
-         'o2': {'o3': {'_ignore': 'a', 'inner':1}, 'o4':{'inner':2},
-         "r1": ["before", "o3", "o4"]}}
+    o = {'ob1': {'x':1, 'y': 1}, 
+         'ob2': {'ob3': {'_ignore': 'a', 'inner':1}, 'ob4':{'inner':2},
+         "r1": ["before", "ob3", "ob4"]}}
 
-    print(flattenJSON(o))
+    fo = flattenJSON(o)
+    print(fo)
+
+    so = structurizeJSON(fo)
+    print(so)
+
