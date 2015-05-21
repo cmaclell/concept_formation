6870322082e431b32cf5f3c0d5e4240b1c22dcb2
Chris MacLellan <maclellan.christopher@gmail.com>
2015-05-21 19:36:23 -0400

    incremented version number in changes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='concept_formation',
-    version='0.1.0',
+    version='0.1.1',
     author='Christopher J. MacLellan, Erik Harpstead',
     author_email='maclellan.christopher@gmail.com, whitill29@gmail.com',
     packages=['concept_formation', 
