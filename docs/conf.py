bb72e287b0586d5cbf495f675762263eb05f8f9e
Chris MacLellan <maclellan.christopher@gmail.com>
2015-05-24 13:45:03 -0400

    tyring to get read the docs to compile

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,15 @@
 import sys
 import os
 import shlex
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['ipython']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
