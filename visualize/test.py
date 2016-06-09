91c7108a83a9cfa99a1691f1175a366f2618f2a3
eharpste <eharpste@cs.cmu.edu>
2016-06-09 11:46:50 -0400

    expanded the visualization to better integrate image files and other features

--- visualize/test.py
+++ visualize/test.py
@@ -8,12 +8,12 @@ from concept_formation.trestle import TrestleTree
 from concept_formation.preprocessor import ObjectVariablizer
 
 
-def output_json(file="forest_fire",size=100,prune=True,seed=50,burn=1):
+def output_json(file="forest",size=100,prune=True,seed=50,burn=1):
     random.seed(seed)
-    if file == "forest_fires":
+    if file == "forest":
         instances = ds.load_forest_fires()
         variables = False
-    elif file == "congressional_voting":
+    elif file == "voting":
         instances = ds.load_congressional_voting()
         variables = False
     elif file == "iris":
@@ -57,7 +57,8 @@ def output_json(file="forest_fire",size=100,prune=True,seed=50,burn=1):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Output example files for testing Viz.')
-    parser.add_argument('file',choices=["forest_fires","congressional_voting","iris","mushroom","rb_com_11","rb_s_07","rb_s_13"],help="which example dataset to use")
+    parser.add_argument('-file',choices=["forest","voting","iris","mushroom","rb_com_11","rb_s_07","rb_s_13"],
+                            default="forest",help="which example dataset to use")
     parser.add_argument('-size',type=int,default=100,help='how many instances to use')
     parser.add_argument('-prune',action='store_true',help='whether to output the tree with pruning applied.')
     parser.add_argument('-seed',type=int,default=50,help='seed to use when shuffling instances for training.')
