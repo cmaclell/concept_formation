bd79f3dc114d3b75894abdcf3aaeba756cc52f0a
Chris MacLellan <maclellan.christopher@gmail.com>
2016-07-26 18:20:29 -0400

    updated dummy tree to use structure mapper correctly after some changes to preprocessor.

--- concept_formation/dummy.py
+++ concept_formation/dummy.py
@@ -17,15 +17,13 @@ class DummyTree(TrestleTree):
     The DummyTree is designed to serve as a naive baseline to compare
     :class:`Trestle <concept_formation.trestle.TrestleTree>` to. The DummyTree
     can perform :meth:`structure mapping
-    <concept_formation.structure_mapper.StructureMapper.transform>` but in all other
-    respects it is effectively a tree that consists of only a root.
+    <concept_formation.structure_mapper.StructureMapper.transform>` but in all
+    other respects it is effectively a tree that consists of only a root.
     """
 
     def __init__(self):
         self.root = Cobweb3Node()
         self.root.tree = self
-        self.alpha = 0
-        self.scaling = False
 
     def ifit(self, instance, do_mapping=False):
         """
@@ -34,24 +32,23 @@ class DummyTree(TrestleTree):
         The structure_map parameter determines whether or not to do structure
         mapping. This is disabled by default to get a really naive model.
 
-        **This process modifies the tree's knoweldge.** For a non-modifying version
-        see: :meth:`DummyTree.categorize`.
+        **This process modifies the tree's knoweldge.** For a non-modifying
+        version see: :meth:`DummyTree.categorize`.
 
         :param instance: an instance to be categorized into the tree.
         :type instance: {a1:v1, a2:v2, ...}
         :param do_mapping: a flag for whether or not to do structure mapping.
         :type do_mapping: bool
-        :return: the root node of the tree containing everything ever added to it.
+        :return: the root node of the tree containing everything ever added to
+            it.
         :rtype: Cobweb3Node
         """
         if do_mapping:
-            structure_mapper = StructureMapper(self.root)
-            temp_instance = structure_mapper.transform(instance)
+            pipeline = Pipeline(SubComponentProcessor(), Flattener(),
+                                StructureMapper(self.root, gensym=self.gensym))
         else:
-            pipeline = Pipeline(Tuplizer(), ListProcessor(),
-                                 NameStandardizer(),
-                                 SubComponentProcessor(), Flattener())
-            temp_instance = pipeline.transform(instance)
+            pipeline = Pipeline(SubComponentProcessor(), Flattener())
+        temp_instance = pipeline.transform(instance)
         self.root.increment_counts(temp_instance)
         return self.root
 
@@ -60,8 +57,8 @@ class DummyTree(TrestleTree):
         Return the root of the tree. Because the DummyTree contains only 1 node
         then it will always categorize instances to that node.
 
-        **This process does not modify the tree's knoweldge.** For a modifying version
-        see: :meth:`DummyTree.ifit`.
+        **This process does not modify the tree's knoweldge.** For a modifying
+        version see: :meth:`DummyTree.ifit`.
         
         :param instance: an instance to be categorized into the tree.
         :type instance: {a1:v1, a2:v2, ...}
