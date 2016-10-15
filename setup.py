1b2b3334e3f074c2823a180364a41467ac0c5c5b
Chris MacLellan <maclellan.christopher@gmail.com>
2016-10-15 14:27:09 -0400

    updated pysearch requirement

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     license='LICENSE.txt',
     description='A library for doing incremental concept formation using algorithms in the COBWEB family.',
     long_description=open('README.rst').read(),
-    install_requires=['py_search>=1.0.2', 'munkres>=1.0.8'],
+    install_requires=['py_search>=1.0.4', 'munkres>=1.0.8'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest','matplotlib','numpy','scikit-learn','scipy'],
 )
