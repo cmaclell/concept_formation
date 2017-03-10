3ae96c16f41d584bcc9b54df71878ad2d0bc6a97
eharpste <eharpste@cs.cmu.edu>
2017-03-10 01:04:21 -0500

    created a differnet method for agglomeration that is based more on how trestle normally works. Nodes are merged into eacother to maximize CU at a given node before iterating down into children to repeat the process. It seems reasonably robust at the moment but still not sure its correct

--- concept_formation/agglomerate.py
+++ concept_formation/agglomerate.py
@@ -74,6 +74,100 @@ def glom(instances):
 
     return tree
 
+def init_tree(instances):
+    nodes = []
+    for i in instances:
+        is_match = False
+        for c in nodes:
+            if c.is_exact_match(i):
+                c.increment_counts(i)
+                is_match = True
+                break
+        if not is_match:
+            c = Cobweb3Node()
+            c.increment_counts(i)
+            nodes.append(c)
+    tree = Cobweb3Tree()
+
+    for n in nodes:
+        tree.root.update_counts_from_node(n)
+        tree.root.children.append(n)
+        n.tree = tree
+        n.parent = tree.root
+    return tree
+
+def cu_for_merge_into(node,c1,c2):
+    temp = node.shallow_copy()
+    t1 = c1.shallow_copy()
+    t1.update_counts_from_node(c2)
+    temp.children.append(t1)
+
+    for c in node.children:
+        if c == c1 or c == c2:
+            continue
+        temp.children.append(c.shallow_copy())
+
+    return temp.category_utility()
+
+def merge_into(parent, c1, c2):
+    if len(c1.children) == 0:
+        c1.create_child_with_current_counts()
+    c1.update_counts_from_node(c2)
+    parent.children.remove(c2)
+    c1.children.append(c2)
+    c2.parent = c1
+    return c1
+
+def get_options(parent,new_child=None):
+    if len(parent.children) <= 2:
+            return []
+    options = []
+    if new_child is None:
+        for i in range(len(parent.children)):
+            c1 = parent.children[i]
+            for j in range(i+1,len(parent.children)):
+                c2 = parent.children[j]
+                cu = cu_for_merge_into(parent,c1,c2)
+                options.append((cu,c1,c2))
+        return options
+    else:
+        for c2 in parent.children:
+            if c2 == new_child:
+                continue
+            cu = cu_for_merge_into(parent,new_child,c2)            
+            options.append((cu,new_child,c2))
+        return options
+
+
+def merge_at_node(node):
+    if len(node.children) <= 2:
+        return
+
+    ops = get_options(node)
+    print([o[0] for o in ops])
+    while len(ops) > 0:
+        ops.sort(reverse=True)
+        cu_opt, c1, c2 = ops[0]
+        cu_curr = node.category_utility()
+        if cu_opt > cu_curr:
+            new_node = merge_into(node,c1,c2)
+
+            ops = [op for op in ops if op[1] != c2 and op[2] != c2]
+            ops += get_options(node,new_node)
+        else:
+            break
+
+    for child in node.children:
+        merge_at_node(child)
+
+
+
+
+def glom2(instances):
+    tree = init_tree(instances)
+    merge_at_node(tree.root)
+    return tree
+
 if __name__ == '__main__':
     from concept_formation.datasets import load_mushroom
     from random import normalvariate, uniform, shuffle, seed
@@ -86,8 +180,8 @@ if __name__ == '__main__':
     num_samples = 30
     sigma =1
 
-    xmean = [12,0,-9,-20]
-    ymean = [15,-8,-20,0]
+    xmean = [6,0,-9,-3]
+    ymean = [7,-2,-8,0]
 
     # xmean = [uniform(-20, 20) for i in range(num_clusters)]
     # ymean = [uniform(-20, 20) for i in range(num_clusters)]
@@ -101,7 +195,8 @@ if __name__ == '__main__':
                  range(num_samples)]
 
     # data = load_mushroom()[:50]
-    tree = glom(data)
+    tree = glom2(data)
+    # tree = init_tree(data)
     visualize(tree)
     # clus = cluster_split_search(tree,data,CU,1,40,False,True,True)
     # visualize_clusters(tree,clus)
