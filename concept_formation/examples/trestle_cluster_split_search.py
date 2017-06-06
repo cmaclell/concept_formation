9586ac41b129313a67f11e877a9056ff73a9b57c
eharpste <eharpste@cs.cmu.edu>
2017-06-06 11:29:47 -0400

    modified the cluster_split_search example to show 3 different datasets to get a better sense of performance compared to human labeling.

--- concept_formation/examples/trestle_cluster_split_search.py
+++ concept_formation/examples/trestle_cluster_split_search.py
@@ -13,34 +13,40 @@ from concept_formation.trestle import TrestleTree
 from concept_formation.cluster import cluster_split_search
 from concept_formation.cluster import AIC, BIC, AICc, CU
 from concept_formation.datasets import load_rb_wb_03
+from concept_formation.datasets import load_rb_com_11
+from concept_formation.datasets import load_rb_s_13
 from concept_formation.preprocessor import ObjectVariablizer
 
 seed(5)
 
-towers = load_rb_wb_03()
-shuffle(towers)
-towers = towers[:60]
-
-variablizer = ObjectVariablizer()
-towers = [variablizer.transform(t) for t in towers]
-
-tree = TrestleTree()
-tree.fit(towers)
-
 hueristics = [AIC,BIC,CU,AICc]
 
-clusters = [cluster_split_search(tree,towers,h,minsplit=1,maxsplit=40,mod=False) for h in hueristics]
-human_labels = [tower['_human_cluster_label'] for tower in towers]
+def calculate_aris(dataset):
+    shuffle(dataset)
+    dataset = dataset[:60]
+
+    variablizer = ObjectVariablizer()
+    dataset = [variablizer.transform(t) for t in dataset]
+
+    tree = TrestleTree()
+    tree.fit(dataset)
+
+    clusters = [cluster_split_search(tree,dataset,h,minsplit=1,maxsplit=40,mod=False) for h in hueristics]
+    human_labels = [dataset['_human_cluster_label'] for dataset in dataset]
+
+    return [max(adjusted_rand_score(human_labels, huer),0.01) for huer in clusters]
 
 x = np.arange(len(hueristics))
-y = [max(adjusted_rand_score(human_labels, huer),0.01) for huer in clusters]
-width = 0.45
+width = 0.3
 
 hueristic_names = ['AIC','BIC','CU','AICc']
-for i in range(len(clusters)):
-    hueristic_names[i] +=  '\nClusters='+str(len(set(clusters[i])))
+# for i in range(len(clusters)):
+#     hueristic_names[i] +=  '\nClusters='+str(len(set(clusters[i])))
 
-plt.bar(x,y,width,color='r',alpha=.8,align='center')
+b1 = plt.bar(x-width,calculate_aris(load_rb_wb_03()),width,color='r',alpha=.8,align='center')
+b2 = plt.bar(x,calculate_aris(load_rb_com_11()),width,color='b',alpha=.8,align='center')
+b3 = plt.bar(x+width,calculate_aris(load_rb_s_13()),width,color='g',alpha=.8,align='center')
+plt.legend((b1[0],b2[0],b3[0]),('wb_03','com_11','s_13'))
 plt.title("TRESTLE Clustering Accuracy of Best Clustering by Different Hueristics")
 plt.ylabel("Adjusted Rand Index (Agreement Correcting for Chance)")
 plt.ylim(0,1)
