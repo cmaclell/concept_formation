386e199abc5641471720797aa19427b7627ecc7f
cmaclell <maclellan.christopher@gmail.com>
2019-01-20 13:18:49 -0500

    moving closer to using tox with travis and coveralls.

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name='concept_formation',
-    version='0.3.4',
-    author='Christopher J. MacLellan, Erik Harpstead',
-    author_email='maclellan.christopher@gmail.com, whitill29@gmail.com',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    url='https://pypi.python.org/pypi/concept_formation/',
-    license='MIT License',
-    description=('A library for doing incremental concept formation '
-                 'using algorithms in the COBWEB family.'),
-    long_description=open('README.rst').read(),
-    install_requires=['py_search>=2.0.0', 'munkres>=1.0.12'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'matplotlib', 'numpy', 'scikit-learn', 'scipy'],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Science/Research',
-                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
-                 'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: Implementation :: PyPy'],
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
 )
