de4b8a2ff2bf795b565ea9a9b92c4da925c95d2e
Christopher MacLellan <cmaclell@gatech.edu>
2022-12-30 13:05:30 -0500

    got working version of code that can compile cpp modules, currently working for continuous_values module

--- setup.py
+++ setup.py
@@ -7,8 +7,28 @@ from pybind11.setup_helpers import build_ext
 ext_modules = intree_extensions(glob('concept_formation/*.cpp'))
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name="concept_formation",
+    author="Christopher J. MacLellan, Erik Harpstead",
+    author_email="maclellan.christopher@gmail.com, whitill29@gmail.com",
+    url="https://github.com/cmaclell/concept_formation",
+    description="A library for doing incremental concept formation using"
+                "algorithms in the COBWEB family.",
+    long_description=open('README.rst').read(),
+    description_content_type="text/x-rst; charset=UTF-8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ],
+    keywords="clustering,machine-learning",
+    license="MIT",
+    license_file="LICENSE.txt",
+    packages=["concept_formation"],
     ext_modules=ext_modules,
     extras_require={"test": "pytest"},
     cmdclass={"build_ext": build_ext},
