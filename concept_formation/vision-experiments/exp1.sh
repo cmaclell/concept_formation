db84b53ba4da97a9a91a68371583e505c4c02ea1
xinthelian <xinthelian@hotmail.com>
2023-08-10 23:30:51 -0400

    Shell script for experiment sets

---
+++ concept_formation/vision-experiments/exp1.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+# 0
+python3 main_mnist.py --experiment=1 --label=0 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=0 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=0 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=0 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=0 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=0 --model-type='fc-cnn' --seed=50
+
+# 1
+python3 main_mnist.py --experiment=1 --label=1 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=1 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=1 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=1 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=1 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=1 --model-type='fc-cnn' --seed=50
+
+# 2
+python3 main_mnist.py --experiment=1 --label=2 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=2 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=2 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=2 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=2 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=2 --model-type='fc-cnn' --seed=50
+
+# 3
+python3 main_mnist.py --experiment=1 --label=3 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=3 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=3 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=3 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=3 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=3 --model-type='fc-cnn' --seed=50
+
+# 4
+python3 main_mnist.py --experiment=1 --label=4 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=4 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=4 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=4 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=4 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=4 --model-type='fc-cnn' --seed=50
+
+# 5
+python3 main_mnist.py --experiment=1 --label=5 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=5 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=5 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=5 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=5 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=5 --model-type='fc-cnn' --seed=50
+
+# 6
+python3 main_mnist.py --experiment=1 --label=6 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=6 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=6 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=6 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=6 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=6 --model-type='fc-cnn' --seed=50
+
+# 7
+python3 main_mnist.py --experiment=1 --label=7 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=7 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=7 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=7 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=7 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=7 --model-type='fc-cnn' --seed=50
+
+# 8
+python3 main_mnist.py --experiment=1 --label=8 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=8 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=8 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=8 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=8 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=8 --model-type='fc-cnn' --seed=50
+
+# 9
+python3 main_mnist.py --experiment=1 --label=9 --model-type='cobweb' --seed=123
+python3 main_mnist.py --experiment=1 --label=9 --model-type='cobweb' --seed=456
+python3 main_mnist.py --experiment=1 --label=9 --model-type='cobweb' --seed=789
+python3 main_mnist.py --experiment=1 --label=9 --model-type='cobweb' --seed=0
+python3 main_mnist.py --experiment=1 --label=9 --model-type='cobweb' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc' --seed=123
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc' --seed=456
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc' --seed=789
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc' --seed=0
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc' --seed=50
+
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc-cnn' --seed=123
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc-cnn' --seed=456
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc-cnn' --seed=789
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc-cnn' --seed=0
+python3 main_mnist.py --experiment=1 --label=9 --model-type='fc-cnn' --seed=50
